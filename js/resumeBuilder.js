4cd78ed47613e7727c6c920592454c486aad262c
menachemkorf <menachemkorf@gmail.com>
2015-01-21 17:14:50 +1100

    added bio, work and education objects to for resume

---
+++ js/resumeBuilder.js
@@ -0,0 +1,77 @@
+
+
+var bio = {
+    "name" : "Menachem Korf",
+    "role" : "Web Developer",
+    "contact" : {
+        "phone" : "0468425770",
+        "email" : "menachemkorf@gmail.com",
+        "github" : "menachemkorf"
+    },
+    "bioPic" : "images/bio_photo.jpg",
+    "welcomeMessage" : "I'm a passionate Web Developer speciolized in JavaScript"
+};
+
+var work = {
+    "jobs" :
+    [
+        {
+            "position" : "Mashgiach",
+            "employer" : "Yeshiva College",
+            "location" : "Melbourne",
+            "dates" : "2015"
+        },
+            {
+            "position" : "Researcher",
+            "employer" : "Chabad of Midtown",
+            "location" : "New York",
+            "dates" : "2012 - 2014"
+        },
+        {
+            "position" : "Teacher",
+            "employer" : "Mesivta Menachem",
+            "location" : "Staten Island, NY",
+            "dates" : "2009"
+        }
+    ]
+};
+
+
+
+var education = {
+    "schools" : [
+        {
+            "name" : "Chabad of Midtown Kollel",
+            "subjects" : ["Rabbinic Studies", "Chassidus"], 
+            "dates" : "2012 - 2014",
+            "location" : "New York"
+        },
+        {
+            "name" : "Crown Hieghts Kollel",
+            "subjects" : ["Rabbinic Studies"], 
+            "dates" : "2011 - 2012",
+            "location" : "New York"
+        }
+    ],
+    "online" : [
+        {
+            "name" : "Treehouse",
+            "subjects" : ["PHP", "JQuery"], 
+            "dates" : "2014",
+            "URL" : "http://teamtreehouse.com/library/build-a-simple-php-application"
+        },
+        {
+            "name" : "Udacity",
+            "subjects" : ["HTML", "CSS", "Bootstrap"], 
+            "dates" : "2015",
+            "URL" : "https://www.udacity.com/course/nd001"
+        },
+        {
+            "name" : "Udacity",
+            "subjects" : ["JavaScript"], 
+            "dates" : "2015",
+            "URL" : "https://www.udacity.com/course/nd001"
+        }
+    ]
+};
+
